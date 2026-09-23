2c6eb8b2797cc7d0bd2b737ed7cf1960093bcd87
Atharv Yuvraj Patil <atharvyp12@gmail.com>
2026-09-23 14:04:27 +0530

    Create orders schema

---
+++ backend/schemas/OrdersSchema.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const OrdersSchema = new Schema({
+    name: String,
+    qty: Number,
+    price: Number,
+    model: String,
+})
+
+module.exports={OrdersSchema};
