a0a4e23347998ebcdb9921fdf1530645478cdb7f
Atharv Yuvraj Patil <atharvyp12@gmail.com>
2026-09-25 15:05:45 +0530

    Add API routes to fetch holdings and positions

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,51 @@
+require('dotenv').config();
 const express = require("express");
+const mongoose = require('mongoose');
+const PORT=process.env.PORT || 3002;
+const uri = process.env.MONGO_URL;
 const app = express();
+const cors = require("cors");
+app.use(cors());
+
+app.use(express.json());
+
+// const { watchlist, holdings, positions } = require("../dashboard/src/data/data.js");
+
+
+const { HoldingsModel } = require("./model/HoldingsModel");
+const { PositionsModel } = require("./model/PositionsModel");
+
+// app.get("/addPositions", async (req, res) => {
+//   positions.forEach((item) => {
+//     let newpositions = new PositionsModel({
+//       product: item.product,
+//       name: item.name,
+//       qty: item.qty,
+//       avg: item.avg,
+//       price: item.price,
+//       net: item.net,
+//       day: item.day,
+//       isLoss: item.isLoss,
+//     });
+
+//     newpositions.save();
+//   });
+//   res.send("Done!");
+// });
+
+app.get('/allHoldings',async(req,res)=>{
+    let allHoldings= await HoldingsModel.find({});
+    res.json(allHoldings);
+})
+
+app.get('/allPositions',async(req,res)=>{
+    let allPositions= await PositionsModel.find({});
+    res.json(allPositions);
+})
+
 
 app.listen(3002,()=>{
     console.log("app is started");
+    mongoose.connect(uri);
+    console.log("Db is started");
 });
