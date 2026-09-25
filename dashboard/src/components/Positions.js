f623ec107b7f99970e6723ace33b44ad772c8382
Atharv Yuvraj Patil <atharvyp12@gmail.com>
2026-09-25 15:29:49 +0530

    feat: fetch positions data from backend API

--- dashboard/src/components/Positions.js
+++ dashboard/src/components/Positions.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React ,{useState,useEffect} from "react";
 
-import { positions } from "../data/data";
+// import { positions } from "../data/data";
+import axios from "axios";
 
 const Positions = () => {
+
+    const [allPositions,setAllPostions] = useState([]);
+      
+  useEffect(()=>{
+    axios.get('http://localhost:3002/allPositions').then((res)=>{
+      console.log(res.data);
+      setAllPostions(res.data);
+    })
+
+  })
+
   return (
     <>
       <h3 className="title">Positions (2)</h3>
@@ -19,7 +31,7 @@ const Positions = () => {
             <th>Chg.</th>
           </tr>
 
-          {positions.map((stock, index) => {
+          {allPositions.map((stock, index) => {
             const curValue = stock.price * stock.qty;
             const isProfit = curValue - stock.avg * stock.qty >= 0.0;
             const profClass = isProfit ? "profit" : "loss";
