90dbe1b8135b85527d346c37b64402e879f2f72e
Atharv Yuvraj Patil <atharvyp12@gmail.com>
2026-09-26 12:08:42 +0530

    fix: update OrdersModel.js

--- backend/model/OrdersModel.js
+++ backend/model/OrdersModel.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
+
 const { OrdersSchema } = require("../schemas/OrdersSchema");
 
-const OrdersModel = mongoose.model("holding", HoldingsSchema);
+const OrdersModel = mongoose.model("Order", OrdersSchema);
+
 module.exports = { OrdersModel };
